8341afe9446e8b1e52e683c4c3a4d8e2b9a5fb4e
perpsdev <perpsdev@gmail.com>
2025-08-23 21:25:25 +0100

    [V2.03] | various patches

--- update.sh
+++ update.sh
@@ -6,11 +6,7 @@ read -p "update notes: " notes
 
 #stage, commit, push
 git add .
-git commit -m "$notes"
+git commit -m "[$version] | $notes"
 git push
-
-# create git tag for version
-git tag "$version"
-git push origin "$version"
 
 echo "updated site: \"$notes\" and tagged as $version B]"
