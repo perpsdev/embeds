c5f66b3bc638891a93dba5187ae8ab1909b3b7d0
perpsdev <perpsdev@gmail.com>
2025-08-24 19:45:51 +0100

    [V3] | new html game, 4 new embeds

--- update.sh
+++ update.sh
@@ -3,15 +3,18 @@
 # go to the repo folder
 cd ~/Documents/embeds || { echo "Repo folder not found!"; exit 1; }
 
+# read token from file
+TOKEN=$(cat ~/Documents/git\ token.txt)
+
 # ask version number
 read -p "site version: " version
-
-# option message
 read -p "update notes: " notes
 
-# stage, commit, push
+# stage, commit
 git add .
 git commit -m "[$version] | $notes"
-git push origin main
+
+# push using token
+git push https://$TOKEN@github.com/your-username/embeds.git main
 
 echo "✅ Updated site: \"$notes\" and tagged as $version B]"
